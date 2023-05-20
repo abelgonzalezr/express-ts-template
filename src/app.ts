2a68ca08748002b0754fabc099acb839d10724fd
Abel Gonzalez <abelgr45@gmail.com>
2023-05-20 12:01:42 -0400

    add not found Handler

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,13 @@ app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
   });
 RegisterRoutes(app);
 
+app.use(function notFoundHandler(_req, res: ExResponse) {
+  res.status(404).send({
+    message: "Not Found",
+  });
+});
+
+
 app.use(function errorHandler(
     err: unknown,
     req: ExRequest,
